f07bac9925267ac12c588963d37e12024b202e83
darshilbabel <darshilbabel3@gmail.com>
2024-11-08 17:31:45 +0530

    minor changes

--- shikshalokam_mohini/shikshalokam_mohini/settings.py
+++ shikshalokam_mohini/shikshalokam_mohini/settings.py
@@ -104,7 +104,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
 
 ROOT_URLCONF = 'shikshalokam_mohini.urls'
