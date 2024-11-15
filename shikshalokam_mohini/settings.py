bd5382058faeb0bb837c6f43aca4f77b951ce1f9
darshilbabel <darshilbabel3@gmail.com>
2024-11-15 13:37:35 +0530

    minor change

--- shikshalokam_mohini/settings.py
+++ shikshalokam_mohini/settings.py
@@ -214,7 +214,7 @@ INTERNAL_IPS = [
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 50000
 SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 
-CSRF_TRUSTED_ORIGINS = ['https://*.shikshalokam.org', 'https://*.127.0.0.1', 'https://*.65.1.97.8']
+CSRF_TRUSTED_ORIGINS = ['https://*.shikshalokam.org', 'https://*.127.0.0.1', 'https://*.gritworks.ai']
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
