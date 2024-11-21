f78d091ccecc8f9ee4b8a1e629e4cc0d93167844
darshilbabel <darshilbabel3@gmail.com>
2024-11-21 11:37:46 +0530

    minor changes

--- shikshalokam_mohini/settings.py
+++ shikshalokam_mohini/settings.py
@@ -84,7 +84,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'drf_yasg',
     'tailwind',
     'django_extensions',
     'rest_framework',
@@ -265,7 +264,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
 
 SPECTACULAR_SETTINGS = {
