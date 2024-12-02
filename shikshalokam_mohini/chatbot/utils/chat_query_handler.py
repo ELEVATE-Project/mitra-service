1ec3a85e742cc2adbb360d5f846621c152f7b871
KUNALTEMPEST <kunalpratapsinghofficial@gmail.com>
2024-12-02 09:18:30 +0530

    mitra work

---
+++ shikshalokam_mohini/chatbot/utils/chat_query_handler.py
@@ -0,0 +1,90 @@
+import requests
+import os
+from typing import List
+from chatbot.llm_models.llm_script import handle_bedrock_model
+
+DATABASE_INTERFACE_BEARER_TOKEN = os.getenv('DATABASE_INTERFACE_BEARER_TOKEN')
+
+
+def query_database(query_prompt: str, priority_filter: str, limit: int):
+    """
+    Query vector database to retrieve chunk with user's input questions.
+    """
+    url = "http://13.202.158.115:8000/query/"
+    headers = {
+        "Content-Type": "application/json",
+        "accept": "application/json",
+    }
+    data = {
+        "query": query_prompt,
+        "search_limit": limit,
+    }
+    if priority_filter:
+        data["priority_filter"] = priority_filter
+    print("DATA: ", data)
+    response = requests.post(url, json=data, headers=headers)
+
+    if response.status_code == 200:
+        result = response.json()
+        # process the result
+        return result
+    else:
+        print(f"Error: {response.status_code} : {response.content}")
+
+
+def apply_prompt_template(question: str) -> str:
+    """
+        A helper function that applies additional template on user's question.
+        Prompt engineering could be done here to improve the result. Here I will just use a minimal example.
+    """
+    prompt = f"""
+        Based on the above data (if applicable) please answer to following question/greeting: 
+        {question}
+
+        REMEMBER STRICTLY DO NOT PROVIDE ANY INFORMATION WHICH IS OUTSIDE OF CONTEXT AVAILABLE TO YOU.
+    """
+    return prompt
+
+
+def call_bedrock_api(prompt, messages, temperature, chunks: List[str]):
+    """
+    Call chatgpt api with user's question and retrieved chunks.
+    """
+    text_to_add = " Use the following chunks along with the other information provided to generate the output:\n"
+    prompt[0]['text'] += text_to_add + ''.join(
+        map(lambda chunk: f"\n{chunk}", chunks)
+    )
+    print(messages)
+
+    response = handle_bedrock_model(
+        system_prompt=prompt, messages=messages, max_token=2048,
+        temperature=temperature
+    )
+
+    return response
+
+
+def ask(messages, user_question, temperature, priority_filter, top_k, prompt):
+    """
+    Handle user's questions.
+    """
+    chunks_response = query_database(query_prompt=user_question, priority_filter=priority_filter, limit=top_k)
+    print("chunks_response", chunks_response)
+    chunks = []
+    if chunks_response and chunks_response["relevant_texts"]:
+        for result in chunks_response["relevant_texts"]:
+            if (
+                    "translated_query" in result
+                    and result["translated_query"] is not None
+                    and len(result["translated_query"]) > 20
+            ):
+                chunks.append(result["translated_query"])
+            elif (
+                    "original_query" in result
+                    and result["original_query"] is not None
+                    and len(result["original_query"]) > 20
+            ):
+                chunks.append(result["original_query"])
+    print("\nCHUNKS: ", chunks)
+    response = call_bedrock_api(prompt=prompt, messages=messages, temperature=temperature, chunks=chunks)
+    return response, chunks, chunks_response
---
+++ shikshalokam_mohini/chatbot/views/mitra_views.py
@@ -0,0 +1,75 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from chatbot.utils.mitra_base_utils import get_mitra_paraphrase_utils, generate_objective_utils, \
+    generate_action_list_utils, generate_title_utils
+
+
+@api_view(['POST'])
+def paraphrase_view(request):
+    body = request.data
+    user_input = body.get('user_input')
+    print("User Input: ", user_input)
+
+    paraphrased_output = get_mitra_paraphrase_utils(paraphrase_problem=user_input)
+    print("\n\nParaphrased Output: ", paraphrased_output)
+    return Response({
+        'status': 'ok',
+        'paraphrased_output': paraphrased_output
+    }, status=200)
+
+
+@api_view(['POST'])
+def generate_objectives_view(request):
+    body = request.data
+    user_input = body.get('user_input')
+    print("User Input: ", user_input)
+
+    objective_list = generate_objective_utils(user_problem_statement=user_input)
+
+    return Response({
+        'status': 'ok',
+        'objective_list': objective_list
+    }, status=200)
+
+
+@api_view(['POST'])
+def generate_action_list_view(request):
+    body = request.data
+    user_problem_statement = body.get('user_problem_statement')
+    user_objective = body.get('user_objective')
+    print("User Problem Statement: ", user_problem_statement)
+    print("User Objective: ", user_objective)
+
+    input_data = {
+        "user_problem_statement": user_problem_statement,
+        "user_objective": user_objective
+    }
+    action_list = generate_action_list_utils(
+        input_data=input_data
+    )
+
+    return Response({
+        'status': 'ok',
+        'action_list': action_list
+    }, status=200)
+
+
+@api_view(['POST'])
+def generate_title_view(request):
+    body = request.data
+    user_problem_statement = body.get('user_problem_statement')
+    user_objective = body.get('user_objective')
+    user_action_list = body.get('user_action_list')
+    input_data = {
+        "user_problem_statement": user_problem_statement,
+        "user_objective": user_objective,
+        "user_action_list": user_action_list
+    }
+
+    title = generate_title_utils(input_data=input_data)
+
+    return Response({
+        'status': 'ok',
+        'title': title
+    }, status=200)
