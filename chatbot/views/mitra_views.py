c79aae0c4aecdec0ef4d1bfe9fa2e6f63193f9d1
KUNALTEMPEST <kunalpratapsinghofficial@gmail.com>
2024-12-21 13:17:10 +0530

    added language

--- chatbot/views/mitra_views.py
+++ chatbot/views/mitra_views.py
@@ -1,4 +1,7 @@
+import json
+
 from chatbot.models import Profile
+from chatbot.translate.ai4Bharat.text_to_text import call_ai4bharat_translation_api
 from chatbot.utils.shikshalokam_mitra_utils import create_project_utils, create_mitra_project_utils
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -13,9 +16,23 @@ from django.http import JsonResponse
 def paraphrase_view(request):
     body = request.data
     user_input = body.get('user_input')
+    language = body.get('language')
     print("User Input: ", user_input)
 
+    if language !='en':
+        user_input = call_ai4bharat_translation_api(
+            source_language=language, target_language='en', message_body=user_input
+        )
+        print("user_translated_message: ", user_input)
+
     paraphrased_output = get_mitra_paraphrase_utils(paraphrase_problem=user_input)
+
+    if language !='en':
+        paraphrased_output = call_ai4bharat_translation_api(
+            source_language='en', target_language=language, message_body=paraphrased_output
+        )
+        print("llm_translated_message: ", paraphrased_output)
+
     print("\n\nParaphrased Output: ", paraphrased_output)
     return Response({
         'status': 'ok',
@@ -27,10 +44,31 @@ def paraphrase_view(request):
 def generate_objectives_view(request):
     body = request.data
     user_input = body.get('user_input')
+    language = body.get('language')
     print("User Input: ", user_input)
 
-    objective_list, chunk_response = generate_objective_utils(user_problem_statement=user_input)
+    if language !='en':
+        user_input = call_ai4bharat_translation_api(
+            source_language=language, target_language='en', message_body=user_input
+        )
+        print("user_translated_message: ", user_input)
 
+    objective_list, chunk_response = generate_objective_utils(user_problem_statement=user_input)
+    translated_list = None
+    if language !='en':
+        translated_list = call_ai4bharat_translation_api(
+            source_language='en', target_language=language, message_body=json.dumps(objective_list)
+        )
+        if isinstance(translated_list, str):
+            try:
+                translated_list = json.loads(translated_list)
+            except Exception as e:
+                print(e)
+        print("llm_translated_message: ", translated_list)
+    if translated_list:
+        objective_list = translated_list
+    print("type of: ", type(objective_list))
+    print("type of: ", type(translated_list))
     return Response({
         'status': 'ok',
         'objective_list': objective_list,
@@ -43,16 +81,50 @@ def generate_action_list_view(request):
     body = request.data
     user_problem_statement = body.get('user_problem_statement')
     user_objective = body.get('user_objective')
+    language = body.get('language')
     print("User Problem Statement: ", user_problem_statement)
     print("User Objective: ", user_objective)
+
+    if language !='en':
+        user_problem_statement = call_ai4bharat_translation_api(
+            source_language=language, target_language='en', message_body=user_problem_statement
+        )
+        print("user_problem_statement: ", user_problem_statement)
+        user_objective = call_ai4bharat_translation_api(
+            source_language=language, target_language='en', message_body=user_objective
+        )
+        print("user_objective: ", user_objective)
 
     input_data = {
         "user_problem_statement": user_problem_statement,
         "user_objective": user_objective
     }
+
     action_list = generate_action_list_utils(
         input_data=input_data
     )
+
+    if language != 'en':
+        for action_item in action_list:
+            action_steps = action_item.get('actionSteps', [])
+            if action_steps:
+                translated_steps = call_ai4bharat_translation_api(
+                    source_language='en', target_language=language,
+                    message_body=json.dumps(action_steps)
+                )
+
+                if isinstance(translated_steps, str):
+                    try:
+                        translated_steps = json.loads(translated_steps)
+                    except Exception as e:
+                        print("Error parsing translated steps: ", e)
+                        translated_steps = action_steps
+
+                action_item['actionSteps'] = translated_steps
+
+        print("Translated action list: ", action_list)
+
+    print("type of action_list: ", type(action_list))
 
     return Response({
         'status': 'ok',
@@ -66,6 +138,23 @@ def generate_title_view(request):
     user_problem_statement = body.get('user_problem_statement')
     user_objective = body.get('user_objective')
     user_action_list = body.get('user_action_list')
+    language = body.get('language')
+
+    if language !='en':
+        user_problem_statement = call_ai4bharat_translation_api(
+            source_language=language, target_language='en', message_body=user_problem_statement
+        )
+        print("user_problem_statement: ", user_problem_statement)
+        user_objective = call_ai4bharat_translation_api(
+            source_language=language, target_language='en', message_body=user_objective
+        )
+        print("user_objective: ", user_objective)
+        user_action_list = call_ai4bharat_translation_api(
+            source_language=language, target_language='en', message_body=user_action_list
+        )
+        print("user_action_list: ", user_action_list)
+
+
     input_data = {
         "user_problem_statement": user_problem_statement,
         "user_objective": user_objective,
@@ -73,6 +162,15 @@ def generate_title_view(request):
     }
 
     title = generate_title_utils(input_data=input_data)
+
+    if language != 'en':
+        title = call_ai4bharat_translation_api(
+            source_language='en', target_language=language, message_body=title
+        )
+        print("llm_translated_message: ", title)
+
+    print("\n\ntitle Output: ", title)
+
 
     return Response({
         'status': 'ok',
