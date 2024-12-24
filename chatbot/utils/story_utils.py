fcaf12bd8ae2dabbfe20a005884fb387191871c7
darshilbabel <darshilbabel3@gmail.com>
2024-12-24 09:48:17 +0530

    minor changes

--- chatbot/utils/story_utils.py
+++ chatbot/utils/story_utils.py
@@ -88,7 +88,7 @@ def create_story_object(profile_id, session, access_token, flow, model=None):
 
         response_json = handle_bedrock_model(
             system_prompt = prompt_to_use, messages = messages, tools=tool_to_use,
-            temperature=company_bot.bot_temperature, max_token=2048, top_p=company_bot.filter_score
+            temperature=company_bot.bot_temperature, max_token=8192, top_p=company_bot.filter_score
         )
 
         response_json = response_json.replace('\n', '').replace('\t', '').replace(
@@ -266,7 +266,7 @@ def get_end_story_tools():
                                     },
                                     "content": {
                                         "type": "string",
-                                        "description": "Content of the story. MAKE SURE CONTENT GENERATED IS AROUND 600 WORDS"
+                                        "description": "Content of the story. MAKE SURE CONTENT GENERATED IS AROUND 600 WORDS. Also give the story in 2-3 paragraphs."
                                     },
                                     "problem_statement": {
                                         "type": "string",
