accc10e520163f76792ecbb7eecc01d5842dc901
KUNALTEMPEST <kunalpratapsinghofficial@gmail.com>
2024-12-27 22:08:11 +0530

    fix

--- shikshalokam/scripts/template_ingestion.py
+++ shikshalokam/scripts/template_ingestion.py
@@ -8,7 +8,7 @@ from shikshalokam.models.project_vernacular_model import ProjectVernacular
 def ingest_project_template(file_path):
     # file_path = 'shikshalokam/scripts/projectTemplateJson.json'
     json_data = load_json(file_path)
-    results = json_data.get('result')
+    results = json_data
 
     if not results:
         print("No projects found in the provided JSON file.")
@@ -98,7 +98,7 @@ def ingest_project_template(file_path):
 def ingest_task_data(file_path):
     # file_path = 'shikshalokam/scripts/TemplateTask.json'
     json_data = load_json(file_path)
-    results = json_data.get('result')
+    results = json_data
 
     if not results:
         print("No tasks found in the provided JSON file.")
