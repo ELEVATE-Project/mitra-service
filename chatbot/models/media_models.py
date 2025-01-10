fbb0031faa7cf878df0fbd2d7acd1fbbaa4a4c1e
KUNALTEMPEST <kunalpratapsinghofficial@gmail.com>
2025-01-10 16:34:11 +0530

    media vector db work

--- chatbot/models/media_models.py
+++ chatbot/models/media_models.py
@@ -1,16 +1,13 @@
 import json
 import os
 import base64
-
 from celery import shared_task
 from django.db import models
 from chatbot.models import Profile, CompanyBot, MediaTypeChoices
-from django_s3_storage.storage import S3Storage
-
-from chatbot.utils.database_util import upsert_single_file
+from chatbot.utils.database_util import upsert_single_file, delete_single_file
+from shikshalokam.models.enums import PriorityChoices
 
 S3_BASE_URL = os.getenv('S3_MEDIA_URL')
-storage = S3Storage(aws_s3_bucket_name='mohini-static.shikshalokam.org')
 
 
 class ProfileMedia(models.Model):
@@ -39,7 +36,7 @@ class ProfileMedia(models.Model):
 class Media(models.Model):
 
     def get_file_upload_path(self, filename):
-        folder_name = self.company_bot.company.slug
+        folder_name = f'shikshalokam/media/{self.company_bot.id}'
         upload_path = f"{folder_name}/{filename}"
         return upload_path
 
@@ -47,14 +44,11 @@ class Media(models.Model):
     def save_in_vector_db(media_id):
         print('Save in vector for media_id: {}'.format(media_id))
         media = Media.objects.get(id=media_id)
-        media_vector = MediaVector.objects.filter(media=media)
-        media_vector = media_vector[0] if media_vector else None
         kvs = KeyValue.objects.filter(media=media)
         metadata = {
             'source': 'file',
-            'source_id': media_id,
             'url': str(media.url) if media.url is not None else S3_BASE_URL + media.file.name,
-            'company': media.company.slug,
+            'company': media.company_bot.company.slug,
             'created_at': str(media.created_at),
         }
         other_tags = dict()
@@ -66,29 +60,46 @@ class Media(models.Model):
                 metadata[kv.key] = kv.value
             else:
                 other_tags[kv.key] = kv.value
-        if media_vector is not None:
-            metadata['id'] = media_vector.vector_id
         other_tags['s3_link'] = S3_BASE_URL + media.file.name
         metadata['other_tags'] = str(other_tags)
-        status_code, response_text = upsert_single_file(media.name, media.file, metadata, media.media_type)
+
+        with media.file.open("rb") as file:
+            file_content = file.read()
+        file_name = media.file.name.split("/")[-1]
+
+        status_code, response_text = upsert_single_file(file_name, file_content, metadata, media)
         print(status_code, response_text)
-        response_vector = json.loads(response_text)
-        if media_vector is None:
-            media_vector = MediaVector(media=media, vector_id=response_vector['ids'][0])
-            media_vector.save()
+
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         self.save_in_vector_db.apply_async(args=(self.id,), countdown=1)
+
+    @shared_task
+    def delete_from_vector_db(media_id):
+        print('Deleting from vector for media_id: {}'.format(media_id))
+        status_code, response_text = delete_single_file(media_id)
+        print(status_code, response_text)
+        return status_code
+
+    def delete(self, *args, **kwargs):
+        status_code = self.delete_from_vector_db(self.id)
+        if status_code == 200:
+            super().delete(*args, **kwargs)
+        else:
+            raise Exception(
+                f"Failed to delete from vector DB for media_id: {self.id}. Status: {status_code}"
+            )
 
     def get_s3_url(self):
         return f"{S3_BASE_URL}{self.file.name}"
 
     name = models.CharField(max_length=1000)
     url = models.URLField(max_length=1000, null=True, blank=True)
+    priority = models.CharField(max_length=50, default=PriorityChoices.P1, choices=PriorityChoices.choices)
     media_type = models.CharField(max_length=100, choices=MediaTypeChoices.choices, default=MediaTypeChoices.TXT)
     company_bot = models.ForeignKey(CompanyBot, on_delete=models.DO_NOTHING)
-    file = models.FileField(storage=storage, upload_to=get_file_upload_path, max_length=1000)
+    file = models.FileField(upload_to=get_file_upload_path, max_length=1000)
     description = models.TextField(null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
--- chatbot/utils/database_util.py
+++ chatbot/utils/database_util.py
@@ -5,19 +5,40 @@ DATABASE_INTERFACE_BEARER_TOKEN = os.getenv('DATABASE_INTERFACE_BEARER_TOKEN')
 
 SEARCH_TOP_K = 3
 
+base_url = os.getenv('VECTOR_DB_BASE_URL')
 
-def upsert_single_file(filename, file, metadata, file_type):
-    url = "https://demo-mitra.shikshalokam.org/api/upload"
+
+def upsert_single_file(filename, file, metadata, media):
+    url = f"https://{base_url}/api/upload"
 
     payload = {
-        'metadata': json.dumps(metadata)
+        'metadata': json.dumps(metadata),
+        'source_id': str(media.id),
+        'priority': media.priority
     }
     files = [
-        ('file', file)
+        ('file', (filename, file, media.media_type))
     ]
     headers = {
         'accept': 'application/json',
-        'Content-Type': 'application/json'
     }
+    print("payload: ", payload)
     response = requests.request("POST", url, headers=headers, data=payload, files=files)
+    print("upserted: ", response.json())
+    return response.status_code, response.text
+
+
+def delete_single_file(media_id):
+    url = f"https://{base_url}/api/documents"
+
+    payload = json.dumps({
+        'source_id': str(media_id)
+    })
+
+    headers = {
+        'accept': 'application/json',
+    }
+    print("payload: ", payload)
+    response = requests.request("DELETE", url, headers=headers, data=payload)
+    print("deleted: ", response.json())
     return response.status_code, response.text
