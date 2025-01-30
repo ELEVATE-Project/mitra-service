6c3000b7897132985675deae390c981a83f2b693
KUNALTEMPEST <kunalpratapsinghofficial@gmail.com>
2025-01-30 11:48:24 +0530

    Spotlight fix

--- shikshalokam/utils/project_utils.py
+++ shikshalokam/utils/project_utils.py
@@ -1,7 +1,9 @@
+import json
 import traceback
 import os
 import requests
-from shikshalokam.models import Task
+from shikshalokam.models import Task, Project
+
 
 base_url = os.getenv("SHIKSHALOKAM_BASE_URL")
 
@@ -40,8 +42,67 @@ def get_project_formatted_data(user_project):
         'problem_statement': user_project.expected_problem_statement,
         'objective': user_project.expected_objective,
         'action_steps': task_names_str,
-        'duration': user_project.expected_duration + " week"
+        'duration': user_project.expected_duration.strip() + " week"
+        if "week" not in user_project.expected_duration.lower() else user_project.expected_duration
     }
     print("Using project data: ", project_data)
 
     return project_data
+
+
+def check_and_save_project(project_id, access_token, profile):
+    if not Project.objects.filter(project_id=project_id).exists():
+        print(f"Project {project_id} not found. Fetching from API...")
+        fetch_and_save_project(project_id=project_id, access_token=access_token, profile=profile)
+
+
+def fetch_and_save_project(project_id, access_token, profile):
+
+    try:
+        url = f"https://{base_url}/userProjects/details/{project_id}"
+        print("using url: ", url)
+        headers = {
+            "X-auth-token": access_token,
+        }
+        payload = {}
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+        response_json = response.json()
+        print("response: ", response_json)
+
+
+        result = response_json.get("result")
+        project_status = result.get('status', '').upper() if result.get('status') else None
+
+
+
+        project = Project.objects.create(
+            project_id=result.get("_id"),
+            recommended_for= json.dumps(result.get('recommendedFor')),
+            expected_title= result.get('title'),
+            categories= json.dumps(result.get('categories')),
+            expected_duration=result.get('duration'),
+            project_start_date=result.get('startDate'),
+            project_end_date=result.get('endDate'),
+            program_id= result.get('programId'),
+            program_name = result.get('programName'),
+            expected_problem_statement= result.get('programName'),
+            project_status=project_status,
+            project_source = result.get('source'),
+            expected_objective= result.get('description'),
+            author=profile,
+            # "generated_by": ProjectCreatedBy.EXPERT_VETTED
+        )
+        tasks = result.get('tasks', [])
+        for task_data in tasks:
+            Task.objects.get_or_create(
+                project=project,
+                task_id=task_data.get('_id'),
+                defaults={"task_name": task_data.get('name', '')}
+            )
+        print(f"Project {project_id} saved successfully.")
+
+
+    except Exception as e:
+        traceback.print_exc()
+        print(f"Failed to save project: {str(e)}")
