ae52c3e6ae2a7af0d667741a770a22dc3b2c4bf8
darshilbabel <darshilbabel3@gmail.com>
2025-02-11 15:07:04 +0530

    minor change

--- chatbot/utils/bedrock_tool_call.py
+++ chatbot/utils/bedrock_tool_call.py
@@ -45,9 +45,9 @@ def get_bedrock_tool_call_response(system_prompt, messages, company_bot, session
 
     is_function_call = False
     if isinstance(response, dict):
-        tool_use_id = response.get('toolUseId', None)
-        if tool_use_id or response.get('name') == 'get_state_information':
-            is_function_call = True
+        # tool_use_id = response.get('toolUseId', None)
+        # if tool_use_id or response.get('name') == 'get_state_information':
+        is_function_call = True
     print("is_function_call: ", is_function_call)
 
 
