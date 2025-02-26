b15571cd73791ee45e1bbc40d46f1eace0455c73
KUNALTEMPEST <kunalpratapsinghofficial@gmail.com>
2025-02-26 09:25:52 +0530

    fix

--- chatbot/pdf/story_secondpage.py
+++ chatbot/pdf/story_secondpage.py
@@ -1,23 +1,41 @@
 import re
 from shikshalokam.models import Project
+import json_repair
 
 
 def get_story_secondpage_html(story):
+    print("story.action_steps: ", story.action_steps)
+    if isinstance(story.action_steps, str):
+        try:
+            if story.action_steps.strip().startswith("["):
+                story.action_steps = json_repair.repair_json(story.action_steps, return_objects=True)
+                print("story.action_steps after repair: ", story.action_steps)
+            else:
+                story.action_steps = [story.action_steps]
+        except Exception as e:
+            print(f"Error repairing JSON: {e}")
+            story.action_steps = ["No action steps provided."]
+
     action_steps = (
-        story.action_steps.split("\n") if isinstance(story.action_steps, str)
-        else story.action_steps if isinstance(story.action_steps, list)
+        story.action_steps if isinstance(story.action_steps, list)
+        else [story.action_steps] if isinstance(story.action_steps, str)
         else ["No action steps provided."]
     )
     project = Project.objects.filter(story=story).first()
     print("action step type: ", type(action_steps))
     print("action_steps: ", action_steps)
-    steps = action_steps[0]
-    if steps and isinstance(steps, str):
-        split_steps = re.findall(r'(\d+\.\s*[^0-9]+)', steps.strip())
+    # steps = action_steps[0]
+    if action_steps and isinstance(action_steps, list) and len(action_steps) == 1 and isinstance(action_steps[0], str):
+        steps_text = action_steps[0]
+        split_steps = re.findall(r'\d+\.\s*[^0-9]+', steps_text)
         split_steps = [step.strip() for step in split_steps if step.strip()]
-        print("\n\nsplit_steps: ", split_steps)
+    elif action_steps and isinstance(action_steps, str):
+        steps_text = " ".join(action_steps)
+        split_steps = re.findall(r'\d+\.\s*[^.]+', steps_text)
+        split_steps = [step.strip() for step in split_steps if step.strip()]
     else:
-        split_steps = [step.strip() for step in steps if step.strip()]
+        split_steps = [step.strip() for step in action_steps if step.strip()]
+    print("\n\nsplit_steps: ", split_steps)
     steps_html = (
             f"<ol style='list-style-type: none; padding: 0; margin: 0;'>"
             + ''.join(f"<li>{step}</li>" for step in split_steps)
