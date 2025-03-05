604874caf8dc20cd52bca20d9eaf2a91a839ffd8
KUNALTEMPEST <kunalpratapsinghofficial@gmail.com>
2025-03-05 09:49:17 +0530

    Chat openai Integration

--- chatbot/celery_tasks/shikshalokam_bedrock_tasks.py
+++ chatbot/celery_tasks/shikshalokam_bedrock_tasks.py
@@ -1,7 +1,8 @@
 from celery import shared_task
-from chatbot.models import CompanyChat, Profile, CompanyBot, ChatSession
+from chatbot.models import CompanyChat, Profile, CompanyBot, ChatSession, LLMProvider
 from chatbot.models.company_models import CompanyStateMachine
 from chatbot.utils.bedrock_tool_call import get_bedrock_tool_call_response
+from chatbot.utils.chat_utils import get_guided_chat
 
 
 @shared_task
@@ -10,47 +11,17 @@ def get_shikshalokam_bedrock_response(channel_name, session_id, profile_id, rout
     print(session_id)
     chat_session = ChatSession.objects.get(session=session_id)
     profile = Profile.objects.get(id=profile_id)
-    ai_user = Profile.objects.get(id=1)
     company_bot = CompanyBot.objects.get(company=profile.company, route='/')
     state_machine = CompanyStateMachine.objects.get(company_bot=company_bot, step=chat_session.current_step)
     system_context = company_bot.context
-    introductory_message = company_bot.introductory_message
 
-    prompt_to_use = [
-        {
-            # Use 'text' key only for system prompts
-            'text': system_context
-        },
-        {
-            'text': """
-            {}
+    prompt_to_use = get_guided_prompt(
+        company_bot=company_bot, system_context=system_context, state_machine=state_machine
+    )
 
-            Completion Criteria:
-            {}
-            """.format(state_machine.context, state_machine.completion_criteria)
-            # Use 'text' key for system instructions
-        },
-        {
-            'text': company_bot.tool_context
-        }
-    ]
-
-    messages=[]
-    # Bedrock wants user to initiate message first so skipping into mssg
-    for chat in company_chats:
-        if chat.receiver == ai_user:
-            user_message = chat.message
-            if chat.translated_message is not None and chat.translated_message != '':
-                user_message = chat.translated_message
-            messages.append({
-                'role': 'user',
-                'content': [{'text': user_message}]
-            })
-        else:
-            messages.append({
-                'role': 'assistant',
-                "content": [{'text': chat.message}]
-            })
+    messages = get_guided_chat(
+        company_bot=company_bot, company_chats=company_chats
+    )
 
     response = get_bedrock_tool_call_response(
         system_prompt=prompt_to_use, messages=messages, company_bot=company_bot, session_id=session_id,
@@ -58,3 +29,43 @@ def get_shikshalokam_bedrock_response(channel_name, session_id, profile_id, rout
     )
 
     return response
+
+
+def get_guided_prompt(company_bot, system_context, state_machine):
+    prompt_to_use=[]
+    if company_bot.provider == LLMProvider.BEDROCK_CONVERSE:
+        prompt_to_use = [
+            {
+                'text': system_context
+            },
+            {
+                'text': """
+                {}
+
+                Completion Criteria:
+                {}
+                """.format(state_machine.context, state_machine.completion_criteria)
+            },
+            {
+                'text': company_bot.tool_context
+            }
+        ]
+    elif company_bot.provider == LLMProvider.OPENAI:
+        prompt_to_use = [
+            {
+                'role': 'system',
+                'content': """{}
+
+                            {}
+
+                            Completion Criteria:
+                            {}""".format(
+                    system_context,
+                    state_machine.context,
+                    state_machine.completion_criteria
+                )
+            }
+        ]
+
+    return prompt_to_use
+
--- chatbot/utils/chat_utils.py
+++ chatbot/utils/chat_utils.py
@@ -1,19 +1,17 @@
-from chatbot.models import Profile
+from chatbot.models import Profile, LLMProvider
+import json
 
-
-def format_message_as_per_openai_format(chats, prompt=None):
+def format_message_as_per_openai_format(chats):
     ai_user = Profile.objects.get(id=1)
     messages = []
-    if prompt:
-        messages.append({
-            'role': 'system',
-            'content': prompt
-        })
     for chat in chats:
         if chat.receiver == ai_user:
+            user_message = chat.message
+            if chat.translated_message is not None and chat.translated_message != '':
+                user_message = chat.translated_message
             messages.append({
                 'role': 'user',
-                'content': chat.message
+                'content': user_message
             })
         else:
             messages.append({
@@ -21,3 +19,67 @@ def format_message_as_per_openai_format(chats, prompt=None):
                 "content": chat.message
             })
     return messages
+
+
+def format_message_as_per_bedrock_format(chats):
+    ai_user = Profile.objects.get(id=1)
+    messages = []
+    for chat in chats:
+        if chat.receiver == ai_user:
+            user_message = chat.message
+            if chat.translated_message is not None and chat.translated_message != '':
+                user_message = chat.translated_message
+            messages.append({
+                'role': 'user',
+                'content': [{'text': user_message}]
+            })
+        else:
+            messages.append({
+                'role': 'assistant',
+                "content": [{'text': chat.message}]
+            })
+
+    return messages
+
+
+def get_guided_chat(company_bot, company_chats):
+    messages = []
+    if company_bot.provider == LLMProvider.BEDROCK_CONVERSE:
+        messages = format_message_as_per_bedrock_format(chats=company_chats)
+    elif company_bot.provider == LLMProvider.OPENAI:
+        messages = format_message_as_per_openai_format(chats=company_chats)
+
+    return messages
+
+
+def convert_llama_to_openai_tool(llama_tool_call):
+    try:
+        tool_spec = llama_tool_call.get("toolConfig", {}).get("tools", [])[0].get("toolSpec", {})
+
+        if not tool_spec:
+            raise ValueError("Invalid toolSpec structure in the provided Llama tool call.")
+
+        parameters = tool_spec.get("inputSchema", {}).get("json", {})
+
+        if "properties" in parameters:
+            for key, value in parameters["properties"].items():
+                if value.get("type") == "array" and "items" not in value:
+                    value["items"] = {"type": "string"}
+
+        openai_tool = [
+            {
+                "type": "function",
+                "function": {
+                    "name": tool_spec.get("name"),
+                    "description": tool_spec.get("description"),
+                    "parameters": parameters
+                }
+            }
+        ]
+
+        print("Converted OpenAI tool:", json.dumps(openai_tool, indent=4))
+        return openai_tool
+
+    except Exception as e:
+        print("Error converting tool:", str(e))
+        return None
