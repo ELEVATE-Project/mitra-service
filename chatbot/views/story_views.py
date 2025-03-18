d6e29674edabdfbeabcc5f6b866ce9488c5f4996
KUNALTEMPEST <kunalpratapsinghofficial@gmail.com>
2025-03-18 16:54:32 +0530

    Fixes

--- chatbot/views/story_views.py
+++ chatbot/views/story_views.py
@@ -130,9 +130,7 @@ class StoryMediaListCreateView(generics.ListCreateAPIView):
             print("response: ", response)
             print("response status_code: ", response.status_code)
 
-            if (response.status_code == status.HTTP_201_CREATED and access_token not in [None, "", "null"]
-                    and session_value and flow != 'login'):
-                upload_to_cloud(session_value=session_value, access_token=access_token, instance=response.data)
+            if response.status_code == status.HTTP_201_CREATED and flow == 'login':
                 update_story_pdf(
                     access_token=access_token, session=session_value, flow=flow
                 )
@@ -187,13 +185,10 @@ class StoryMediaRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView)
             print("response: ", response)
             print("response status_code: ", response.status_code)
 
-            if response.status_code in [status.HTTP_200_OK, status.HTTP_204_NO_CONTENT]:
-                # Pass response.data directly as the instance
-                upload_to_cloud(session_value=session_value, access_token=access_token, instance=response.data)
-                update_story_pdf(
-                    access_token=access_token, session=session_value, flow=flow
-                )
-
+            if response.status_code in [status.HTTP_200_OK, status.HTTP_204_NO_CONTENT] and flow == 'login':
+                    update_story_pdf(
+                        access_token=access_token, session=session_value, flow=flow
+                    )
             return response
         except Exception as e:
             print("Error occurred: ", str(e))
