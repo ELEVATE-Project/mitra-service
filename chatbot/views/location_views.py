bfbda0e0af234d5e54390ed87d3841b3df108462
KUNALTEMPEST <kunalpratapsinghofficial@gmail.com>
2025-04-08 16:29:10 +0530

    location issue ip

--- chatbot/views/location_views.py
+++ chatbot/views/location_views.py
@@ -45,13 +45,24 @@ def get_location_view(request):
 @api_view(['GET'])
 def get_ip_location_view(request):
     try:
-        url = "http://ip-api.com/json/"
+        # Try to get the user's real IP address from headers
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]  # get the first IP if multiple
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+
+        # Fall back to default IP if IP isn't found
+        if not ip:
+            ip = "127.0.0.1"
+
+        print("Client IP:", ip)
+
+        url = f"http://ip-api.com/json/{ip}"
         response = requests.get(url)
 
         if response.status_code == 200:
-            print("Response: ", response.text)
             location_data = response.json()
-            print("location_data: ", location_data)
 
             location_output = json_repair.repair_json(json.dumps(location_data), return_objects=True)
 
