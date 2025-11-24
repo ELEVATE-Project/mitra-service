2c35ca26149b508052b65de7009455ca8621f58e
darshilbabel <darshilbabel3@gmail.com>
2025-11-24 15:58:16 +0530

    minor fix

--- shikshalokam_mohini/settings.py
+++ shikshalokam_mohini/settings.py
@@ -251,7 +251,7 @@ INTERNAL_IPS = [
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 50000
 SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 
-CSRF_TRUSTED_ORIGINS = ['https://*.shikshalokam.org', 'https://*.127.0.0.1',
+CSRF_TRUSTED_ORIGINS = ['https://*.shikshagraha.org', 'https://*.shikshalokam.org', 'https://*.127.0.0.1',
                         'https://*.gritworks.ai', 'http://localhost:3000']
 
 STORAGES = {
