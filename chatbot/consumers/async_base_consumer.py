aafe2237e3659ec894578ef787b296f1d6ffb51a
Mohit Bharti <mohitbharti373@gmail.com>
2025-12-16 10:24:08 +0530

    Fix disconnect method to ensure connection closure and improve error logging

--- chatbot/consumers/async_base_consumer.py
+++ chatbot/consumers/async_base_consumer.py
@@ -32,6 +32,11 @@ class AsyncBaseConsumer(AsyncWebsocketConsumer):
                     await self.update_last_chat_status_async(chat_status=company_chat_status)
         except Exception as e:
             logger.error('Receive Error: %s', e, exc_info=True)
+        finally:
+            # Always close the connection
+            logger.info('Disconnect stack trace: %s', {traceback.format_stack()}, exc_info=True)
+
+            await self.close()
 
     async def receive(self, text_data):
         raise NotImplementedError("receive method must be implemented in subclass")
