dd80ecf56b72334276eb57adec1222a8a5d2683f
Vishnu Krishnathu <54522897+VishnuKrishnathu@users.noreply.github.com>
2026-01-16 11:34:15 +0530

    Refactor paraphrase_view to improve error handling and logging; update get_mitra_paraphrase_utils to accept messages directly and log paraphrased responses. Replace print statements with logger for better traceability.

--- shikshalokam/utils/mitra_base_utils.py
+++ shikshalokam/utils/mitra_base_utils.py
@@ -1,34 +1,23 @@
 from chatbot.llm_models.llm_script import handle_bedrock_model
 from chatbot.utils.chat_query_handler import ask
 from jinja2 import Template
+import json
+import logging
 
+logger = logging.getLogger('django')
 
-def get_mitra_paraphrase_utils(paraphrase_problem, should_paraphrase_text, company_bot):
-    messages = [{
-                    'role': 'user',
-                    'content': [{'text': paraphrase_problem}]
-                }]
-
+def get_mitra_paraphrase_utils(messages, company_bot):
     paraphrase_prompt = company_bot.context
-    validation_prompt = company_bot.end_context
     paraphrase_prompt = [{'text': paraphrase_prompt}]
-    validation_prompt = [{'text': validation_prompt}]
-    print('validation_prompt: ', validation_prompt)
-    validation_response = handle_bedrock_model(
-        system_prompt=validation_prompt, messages=messages,  model_name = company_bot.llm_model,
-        temperature = company_bot.bot_temperature, max_token = company_bot.max_token, company_bot=company_bot
-    )
-    print("validation_response: ", validation_response)
-    is_validated = validation_response.get('is_validated')
-    if is_validated.lower() == 'no' or not should_paraphrase_text:
-        return validation_response
-    print('paraphrase_prompt: ', paraphrase_prompt)
+
     paraphrase_response = handle_bedrock_model(
         system_prompt=paraphrase_prompt, messages=messages, model_name = company_bot.llm_model,
-        temperature = company_bot.bot_temperature, max_token = company_bot.max_token, company_bot=company_bot
+        temperature = company_bot.bot_temperature, max_token = company_bot.max_token, company_bot=company_bot, tools=json.loads(company_bot.tool_context)
     )
-    print("paraphrase_response: ", paraphrase_response)
-    paraphrase_response = paraphrase_response.get('paraphrased_challenge')
+
+    logger.info("Paraphrased response: %s", json.dumps(paraphrase_response))
+
+    paraphrase_response = paraphrase_response.get('input', {})
     return paraphrase_response
 
 
