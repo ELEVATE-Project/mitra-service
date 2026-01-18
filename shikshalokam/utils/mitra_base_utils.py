04ff9456ffbd339fac609b8cd678affb5fc570eb
KUNALTEMPEST <kunalpratapsinghofficial@gmail.com>
2026-01-18 22:29:04 +0530

    fix in paraphrase

--- shikshalokam/utils/mitra_base_utils.py
+++ shikshalokam/utils/mitra_base_utils.py
@@ -4,6 +4,8 @@ from jinja2 import Template
 import json
 import logging
 
+from shikshalokam.utils.action_list.action_parser import unwrap_tool_values
+
 logger = logging.getLogger('django')
 
 def get_mitra_paraphrase_utils(messages, company_bot):
@@ -17,7 +19,44 @@ def get_mitra_paraphrase_utils(messages, company_bot):
 
     logger.info("Paraphrased response: %s", json.dumps(paraphrase_response))
 
-    paraphrase_response = paraphrase_response.get('input', {})
+    if 'output' in paraphrase_response:
+        content = (
+            paraphrase_response
+            .get('output', {})
+            .get('message', {})
+            .get('content', [])
+        )
+
+        if isinstance(content, list):
+            for item in content:
+                if 'toolUse' in item:
+                    tool_input = item['toolUse'].get('input')
+                    if tool_input:
+                        paraphrase_response = tool_input
+                        break
+
+    extracted_data = (
+            paraphrase_response.pop("parameters", None)
+            or paraphrase_response.pop("input", None)
+    )
+
+    if extracted_data:
+        extracted_data = unwrap_tool_values(extracted_data)
+        paraphrase_response = extracted_data
+
+    logger.info(
+        "Extracted paraphrase data: %s",
+        json.dumps(paraphrase_response, default=str)
+    )
+
+    if isinstance(paraphrase_response, str):
+        try:
+            paraphrase_response = json.loads(paraphrase_response)
+        except Exception:
+            import json_repair
+            paraphrase_response = json_repair.repair_json(
+                paraphrase_response, return_objects=True
+            )
     return paraphrase_response
 
 
