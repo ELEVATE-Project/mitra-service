809973c60cca0e461ce1c4040f3758d14e1c6a84
Vishnu Krishnathu <54522897+VishnuKrishnathu@users.noreply.github.com>
2026-08-03 15:01:03 +0530

    commits reverted

--- shikshalokam_mohini/settings.py
+++ shikshalokam_mohini/settings.py
@@ -364,6 +364,11 @@ if STORAGE_CLOUD_PROVIDER in STORAGE_BACKENDS:
             },
         }
 
+        STORAGES["staticfiles"]["OPTIONS"] = {
+            **STORAGES["staticfiles"]["OPTIONS"],
+            "location": "static",
+        }
+
 else:
     raise ValueError(
         f"Unsupported STORAGE_CLOUD_PROVIDER: {STORAGE_CLOUD_PROVIDER}. "
@@ -527,5 +532,7 @@ CRONJOBS = [
     ('0 12 * * *', 'chatbot.cron_tasks.student_fgd.story_creation.create_story',
     '>> /tmp/student_fgd_story_creation.log 2>&1'),
     ('0 12 * * *', 'chatbot.cron_tasks.xylem.story_creation.create_story',
-    '>> /tmp/xylem_story_creation.log 2>&1')
+    '>> /tmp/xylem_story_creation.log 2>&1'),
+    ('0 */2 * * *', 'chatbot.cron_tasks.guest_discussion.state_categorisation.run_cron',
+    '>> /tmp/guest_discussion_state_categorisation.log 2>&1')
 ]
